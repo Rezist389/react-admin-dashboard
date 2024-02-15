2d14c9803545c536fb40594215b2c9190db024c3
Rezist389 <2020bit033@sggs.ac.in>
2024-02-15 14:23:03 +0530

    vv

---
+++ backend/mongo.js
@@ -0,0 +1,24 @@
+const mongoose=require("mongoose")
+mongoose.connect("mongodb+srv://wrahul2002:pass123@cluster0.cqwum6i.mongodb.net/")
+.then(()=>{
+    console.log("mongodb connected");
+})
+.catch(()=>{
+    console.log('failed');
+})
+
+
+const newSchema=new mongoose.Schema({
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+const collection = mongoose.model("collection",newSchema)
+
+module.exports=collection
